b563e58fd7aca664caac11f1fd7c5ec5cc2a51b3
Arthur Smith <arthursmith@ms-MacBook-Pro.local>
2018-06-05 08:49:16 -0500

    added data structure to case study of PC'

--- src/components/Do.js
+++ src/components/Do.js
@@ -11,9 +11,9 @@ import './parentingproj.css';
 
 let Do = () => {
     return (
-        <div className="mt-5">
-            <h3 className="mb-5 text-center display-3">do.</h3>
-            <h2 className="text-light text-center mt-5">Wireframing</h2>
+        <div className="mt-5 pt-5">
+            <h3 className="mb-5 pb-5 text-center display-3">do.</h3>
+            <h2 className="title mt-5">Wireframing</h2>
 
             <div className="d-flex flex-row flex-wrap justify-content-center mt-5">
                 <div className="card m-2 border-0" style={{width: 20 + 'rem'}}>
@@ -25,7 +25,7 @@ let Do = () => {
                 <div className="card m-2 border-0" style={{width: 20 + 'rem'}}>
                     <img className="card-img-top" src={ScreenSketch3} alt="Card cap" />
                 </div>
-                <p className="text-light mt-3">I still believe that most great ideas start on paper. I start there because paper wireframing gives you a clear goal that you can refere back to at anypoint, it is accessible, and it can catch errors in design fom the start.  </p>
+                <p className="mt-3">I still believe that most great ideas start on paper. I start there because paper wireframing gives you a clear goal that you can refere back to at anypoint, it is accessible, and it can catch errors in design fom the start.  </p>
             </div>
         </div>
     )
