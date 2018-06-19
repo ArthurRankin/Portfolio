d3171afffb3cbc8085ff59435ca93fd2a4a9da98
Arthur Smith <arthursmith@ms-MacBook-Pro.local>
2018-06-19 10:10:25 -0500

    Post-susan notes

---
+++ src/components/ViewPoint.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { EOVERFLOW } from 'constants';
+
+class ViewPoint extends Component {
+    render () {
+        return (
+            <div>
+                <h1>ViewPoint</h1>
+            </div>
+        )
+    }
+}
+
+export default ViewPoint;
